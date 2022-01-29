44a9e48bc95197ae9132d752bb20240fe4e030b3
Nick Chan <towinchenmi@gmail.com>
2022-01-29 15:45:37 +0800

    pnpm

--- update-apps.js
+++ update-apps.js
@@ -1,15 +1,17 @@
+#!/usr/bin/env node
+'use strict'
 if (!process.argv[5]) {
 	console.error("usage: " + process.argv[1] + " <apps.json url> <news.json location> <new apps.json location> <repository url>");
 	process.exit(1)
 }
-const fetch = require("node-fetch")
-const fs = require("fs")
-const news = require(process.argv[3])
+import fetch from "node-fetch"
+import fs from "fs"
+const news = JSON.parse(fs.readFileSync(process.argv[3]));
 fetch(process.argv[2], { headers: { "user-agent": "AltStore/1 CFNetwork/1126 Darwin/19.5.0" }})
 	.then(response => response.json())
 	.then(appsjson => {
 		appsjson.news = news
-		betaApps = []
+		const betaApps = []
 		for (const app of appsjson.apps) {
 			if (app.beta === true) {
 				app.beta = false;
